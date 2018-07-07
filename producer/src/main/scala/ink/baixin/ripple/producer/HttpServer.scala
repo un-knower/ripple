103314803acfc41554550dd2d0083a41460d715e
Trigl <18810690994@163.com>
2018-07-07 17:01:25 +0800

    Finish producer.

--- producer/src/main/scala/ink/baixin/ripple/producer/HttpServer.scala
+++ producer/src/main/scala/ink/baixin/ripple/producer/HttpServer.scala
@@ -1,8 +1,11 @@
 package ink.baixin.ripple.producer
 
 import com.typesafe.scalalogging.Logger
-import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
-import io.netty.handler.codec.http.{DefaultFullHttpResponse, HttpRequest, HttpVersion}
+import io.netty.buffer.Unpooled
+import io.netty.channel.socket.SocketChannel
+import io.netty.channel.{ChannelFutureListener, ChannelHandlerContext, ChannelInitializer, SimpleChannelInboundHandler}
+import io.netty.handler.codec.http._
+import io.netty.handler.timeout.ReadTimeoutHandler
 
 class HttpServerHandler(private val writer: EventWriter)
   extends SimpleChannelInboundHandler[Object] {
@@ -25,6 +28,29 @@ class HttpServerHandler(private val writer: EventWriter)
   override def channelRead0(ctx: ChannelHandlerContext, msg: Object) {
     if (msg.isInstanceOf[HttpRequest]) {
       logger.debug("event=flush_and_finish_requests")
+      val response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NO_CONTENT, Unpooled.EMPTY_BUFFER)
+      ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE)
+      forwardMessage(msg.asInstanceOf[HttpRequest])
     }
+  }
+
+  override def exceptionCaught(ctx: ChannelHandlerContext, error: Throwable) {
+    logger.error(s"event=http_handle_error error=$error")
+    ctx.close
+  }
+
+}
+
+class HttpServerInitializer(private val writer: EventWriter) extends ChannelInitializer[SocketChannel] {
+  private val logger = Logger(this.getClass)
+
+  override def initChannel(ch: SocketChannel) {
+    logger.debug("event=initialize_http_server_channel")
+    ch.pipeline.addLast(
+      new ReadTimeoutHandler(1),
+      new HttpRequestDecoder(8192, 8192, 8192),
+      new HttpResponseEncoder(),
+      new HttpServerHandler(writer)
+    )
   }
 }
